3bcd8a57ad5c8d26480cc320521ae428e5674a4b
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-01-06 20:28:24 -0500

    Added the properties for the board and space objects

--- js/Board.js
+++ js/Board.js
@@ -1,1 +1,28 @@
-class Board {}
+class Board {
+	constructor() {
+		this.rows = 6;
+		this.columns = 7;
+		this.spaces = this.createSpaces(rows, columns);
+	}
+
+	/**
+	 * Creates all the spaces on the board
+	 *
+	 * @param   {integer}   rows number of rows on the board
+	 * @param   {integer}   columns number of columns on the board
+	 * @returns {Array}     An array of all the spaces on the board
+	 */
+
+	createSpaces(rows, columns) {
+		const spaceArray = [];
+
+		for (let i = 0; i < rows; i++) {
+			for (let j = 0; j < columns; j++) {
+				let space = new Space(i, j);
+				spaceArray.push(space);
+			}
+		}
+
+		return spaceArray;
+	}
+}
--- js/Space.js
+++ js/Space.js
@@ -1,1 +1,8 @@
-class Space {}
+class Space {
+	constructor(row, column) {
+		this.row = row;
+		this.column = column;
+		this.id = `space-${row}-${column}`;
+		this.token = null;
+	}
+}
