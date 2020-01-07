745aed068a1af55730e4b843c12f40bcc37fe676
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2020-01-06 20:08:59 -0500

    Created creator method for tokens (#4)

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,22 @@ class Player {
 		this.id = id;
 		this.color = color;
 		this.active = active;
-		this.tokens = [];
+		this.tokens = this.createToken(21);
+	}
+
+	/**
+	 * Creates token objects for player
+	 * @param     {integer}    num - Number of token objects to be created
+	 * @returns   {Array}     An array of the newly created token objects
+	 */
+	createToken(num) {
+		const tokenArray = [];
+
+		for (let i = 0; i < num; i++) {
+			let token = Token(this, i);
+			tokenArray.push(token);
+		}
+
+		return tokenArray;
 	}
 }
