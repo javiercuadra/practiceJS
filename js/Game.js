088bb9cef631a50d3b93b9e6ecda3c5f87be7249
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-01-06 21:08:30 -0500

    created game object

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,21 @@
-class Game {}
+class Game {
+	constructor() {
+		this.players = this.createPlayers();
+		this.board = new Board();
+		this.ready = false;
+	}
+
+	/**
+	 * Creates an array of two players
+	 *
+	 * @returns An array of the two players
+	 */
+
+	createPlayers() {
+		const playerArray = [
+			new Player('Player 1', 1, '#e15258', true),
+			new Player('Player 2', 2, '#e59a13')
+		];
+		return playerArray;
+	}
+}
