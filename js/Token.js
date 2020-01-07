5407f6c3a2eaf3638718fef74da26dbc694d76ea
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-01-07 17:20:45 -0500

    Created rendering methods for Token object

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,22 @@ class Token {
 		this.id = `token-${index}-${owner.id}`;
 		this.dropped = false;
 	}
+
+	/**
+	 * Renders a token object using HTML
+	 */
+	drawHTMLToken() {
+		const token = document.createElement('div');
+		document.getElementById('game-board-underlay').appendChild(token);
+		token.setAttribute('class', 'token');
+		token.setAttribute('id', this.id);
+		token.style.backgroundColor = this.owner.color;
+	}
+
+	/**
+	 * Gets the HTML Token element associated with the Token object
+	 */
+	get htmlToken() {
+		return document.getElementById(this.id);
+	}
 }
