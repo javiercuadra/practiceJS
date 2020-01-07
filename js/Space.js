8db28f1bb63e233690c8192ec61ca6387cd2e25e
javiercuadra <javiercuadra2020@u.northwestern.edu>
2020-01-07 17:19:32 -0500

    Created rendering methods for Space object

--- js/Space.js
+++ js/Space.js
@@ -4,5 +4,26 @@ class Space {
 		this.column = column;
 		this.id = `space-${row}-${column}`;
 		this.token = null;
+		this.diameter = 76;
+		this.radius = this.diameter / 2;
+	}
+
+	/**
+	 * Renders the space object using an svg
+	 */
+	drawSVGSpace() {
+		const svgSpace = document.createElementNS(
+			'http://www.w3.org/2000/svg',
+			'circle'
+		);
+
+		svgSpace.setAttributeNS(null, 'id', this.id);
+		svgSpace.setAttributeNS(null, 'cx', this.x * this.diameter + this.radius);
+		svgSpace.setAttributeNS(null, 'cy', this.y * this.diameter + this.radius);
+		svgSpace.setAttributeNS(null, 'r', this.radius - 8);
+		svgSpace.setAttributeNS(null, 'fill', 'black');
+		svgSpace.setAttributeNS(null, 'stroke', 'none');
+
+		document.getElementById('mask').appendChild(svgSpace);
 	}
 }
