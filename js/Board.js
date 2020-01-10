a8bebc9dc9740fe0ea54d66349b1179f84d19547
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2020-01-10 02:41:37 -0500

    Finished App (#7)
    * connected js files to the DOM
    
    * Created rendering methods for Space object
    
    * Created rendering methods for Board object
    
    * Created rendering methods for Token object
    
    * Created rendering methods for Player object
    
    * Created rendering methods for Game object
    
    * Refectored render methods for Game and Player objects
    
    * rendered game onto screen
    
    * Finished implementation

--- js/Board.js
+++ js/Board.js
@@ -2,7 +2,7 @@ class Board {
 	constructor() {
 		this.rows = 6;
 		this.columns = 7;
-		this.spaces = this.createSpaces(rows, columns);
+		this.spaces = this.createSpaces();
 	}
 
 	/**
@@ -14,15 +14,26 @@ class Board {
 	createSpaces() {
 		const spaceArray = [];
 
-		for (let i = 0; i < this.rows; i++) {
+		for (let x = 0; x < this.columns; x++) {
 			const column = [];
-			for (let j = 0; j < this.columns; j++) {
-				const space = new Space(i, j);
+			for (let y = 0; y < this.rows; y++) {
+				const space = new Space(x, y);
 				column.push(space);
 			}
 			spaceArray.push(column);
 		}
 
 		return spaceArray;
+	}
+
+	/**
+	 * Renders the board object using HTML
+	 */
+	drawHTMLBoard() {
+		for (let column of this.spaces) {
+			for (let space of column) {
+				space.drawSVGSpace();
+			}
+		}
 	}
 }
--- js/Token.js
+++ js/Token.js
@@ -3,5 +3,72 @@ class Token {
 		this.owner = owner;
 		this.id = `token-${index}-${owner.id}`;
 		this.dropped = false;
+		this.columnLocation = 0;
+	}
+
+	/**
+	 * Gets the HTML Token element associated with the Token object
+	 */
+	get htmlToken() {
+		return document.getElementById(this.id);
+	}
+
+	/**
+	 * Gets left offset of html element.
+	 * @return  {number}   Left offset of token object's htmlToken.
+	 */
+	get offsetLeft() {
+		return this.htmlToken.offsetLeft;
+	}
+
+	/**
+	 * Renders a token object using HTML
+	 */
+	drawHTMLToken() {
+		const token = document.createElement('div');
+		document.getElementById('game-board-underlay').appendChild(token);
+		token.setAttribute('class', 'token');
+		token.setAttribute('id', this.id);
+		token.style.backgroundColor = this.owner.color;
+	}
+
+	/**
+	 * Moves html token one column to left.
+	 */
+	moveLeft() {
+		if (this.columnLocation > 0) {
+			this.htmlToken.style.left = this.offsetLeft - 76;
+			this.columnLocation--;
+		}
+	}
+
+	/**
+	 * Moves html token one column to right.
+	 * @param   {number}    columns - number of columns in the game board
+	 */
+	moveRight(columns) {
+		if (this.columnLocation < columns - 1) {
+			this.htmlToken.style.left = this.offsetLeft + 76;
+			this.columnLocation++;
+		}
+	}
+
+	/**
+	 * Drops html token into targeted board space.
+	 * @param   {Object}   target - Targeted space for dropped token.
+	 * @param   {function} reset  - The reset function to call after the drop animation has completed.
+	 */
+
+	dropToken(target, reset) {
+		this.dropped = true;
+
+		$(this.htmlToken).animate(
+			{
+				top: target.y * target.diameter
+			},
+			750,
+			'easeOutBounce',
+			reset
+		);
 	}
 }
